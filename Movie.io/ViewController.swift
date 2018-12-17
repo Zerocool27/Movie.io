b6bfc36a616c6b7a0a83b27785ebe0586a89cc55
fatih <fatih@ebibelot.com>
2018-12-17 11:43:23 -0800

    Initial Commit

---
+++ Movie.io/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie.io
+//
+//  Created by fatih on 12/17/18.
+//  Copyright © 2018 fatih. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
